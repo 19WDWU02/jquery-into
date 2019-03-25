17f9ce26176e8813615066943923a943be5c3f05
Richard Hpa <richard.hpa@hotmail.com>
2019-03-26 10:00:38 +1300

    started to look at jquery

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("external JS is working");
+$(document).ready(function(){
+    console.log("jquery has loaded");
+
+    // $("div").hide();
+    // $("#box2").hide();
+    // $(".box").hide();
+    // $("#box4").show();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
